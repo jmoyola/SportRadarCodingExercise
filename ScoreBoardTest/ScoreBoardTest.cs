2db62c43f0225f058fe12f2f5d2d5206d3f66e29
jmoyola <jmoyola@gmail.com>
2023-01-07 20:58:44 +0100

    ScoreBoardTest: Added Summary_Test

--- ScoreBoardTest/ScoreBoardTest.cs
+++ ScoreBoardTest/ScoreBoardTest.cs
@@ -134,4 +134,68 @@ public class Tests
         
         Debug.WriteLine("ScoreBoard_FinishGame_Test End...");
     }
+    
+    [Test]
+    public void ScoreBoard_Summary_Test()
+    {
+        // Auxiliar gameMatch variable
+        GameMatch auxGameMatch;
+
+        Debug.WriteLine("ScoreBoard_Summary_Test Start...");
+
+        // Populating games and updating score
+        auxGameMatch=this._scoreBoard.StartGame(new Team("Mexico"), new Team("Canada"));
+        this._scoreBoard.UpdateScore(auxGameMatch, 0, 5);
+
+        auxGameMatch=this._scoreBoard.StartGame(new Team("Spain"), new Team("Brazil"));
+        this._scoreBoard.UpdateScore(auxGameMatch, 10, 2);
+
+        auxGameMatch=this._scoreBoard.StartGame(new Team("Germany"), new Team("France"));
+        this._scoreBoard.UpdateScore(auxGameMatch, 2, 2);
+
+        auxGameMatch=this._scoreBoard.StartGame(new Team("Uruguay"), new Team("Italy"));
+        this._scoreBoard.UpdateScore(auxGameMatch, 6, 6);
+        
+        auxGameMatch=this._scoreBoard.StartGame(new Team("Argentina"), new Team("Australia"));
+        this._scoreBoard.UpdateScore(auxGameMatch, 3, 1);
+
+        
+        Debug.WriteLine("ScoreBoard content:"
+                        + Environment.NewLine
+                        + String.Join(Environment.NewLine, this._scoreBoard.GetScoreBoard()));
+
+        // Getting scoreboard summary
+        var summary = this._scoreBoard.GetSummary();
+
+        Debug.WriteLine("ScoreBoard summary:"
+                        + Environment.NewLine
+                        + String.Join(Environment.NewLine, summary));
+
+        
+        bool assert = true;
+        
+        // Iterates over all gameMatchs for index
+        for (int i = 0; i < summary.Count; i++)
+        {
+            // Assert verification for each element (from second to end) 
+            if (assert && i > 0)
+            {
+                // If previous gameMatch totalScore is equal to actual GameMatch totalScore (second order desc by lastUpdateTime)
+                if (summary[i - 1].TotalScore==summary[i].TotalScore)
+                    // Asser is true if previous gameMatch lastUpdateTime is greater than actual GameMatch lastUpdateTime
+                    assert = assert & summary[i - 1].LastUpdateTime > summary[i].LastUpdateTime;
+                else  // first order desc by totalScore
+                    // Asser is true if previous gameMatch totalScore is greater than actual GameMatch totalScore
+                    assert = assert & summary[i-1].TotalScore > summary[i].TotalScore;
+                
+                if(!assert)
+                    break;
+            }
+        }
+        
+        // It's ok if all items (from second to end) are in correct order possition
+        Assert.IsTrue(assert);
+        
+        Debug.WriteLine("ScoreBoard_Summary_Test End...");
+    }
 }
