f3d131f78eb60ee0c48401e1b7f3b068976fa8d9
jmoyola <jmoyola@gmail.com>
2023-01-07 20:56:05 +0100

    ScoreBoardTest: Added StartGame_Test

--- ScoreBoardTest/ScoreBoardTest.cs
+++ ScoreBoardTest/ScoreBoardTest.cs
@@ -23,4 +23,36 @@ public class Tests
         // Setting dbSessionFactory fo dbModel to instance of library
         this._scoreBoard.DbSessionFactory = new DbSessionFactory<ScoreBoardDbModel>(dbModel); 
     }
+
+    [Test]
+    public void ScoreBoard_StartGame_Test()
+    {
+        Team homeTeam;
+        Team awayTeam;
+
+        Debug.WriteLine("ScoreBoard_StartGame_Test Start...");
+        
+        // Creating teams for game
+        homeTeam = new Team("homeTeam");
+        awayTeam = new Team("awayTeam");
+        
+        // Starting game
+        this._scoreBoard.StartGame(homeTeam, awayTeam);
+        
+        Debug.WriteLine("ScoreBoard:"
+                        + Environment.NewLine
+                        + String.Join(Environment.NewLine, this._scoreBoard.GetScoreBoard()));
+
+        // Getting storeboard
+        var sb = this._scoreBoard.GetScoreBoard();
+        
+        // Is ok if count is 1 and exists any game with match in base properties.
+        Assert.IsTrue(sb.Count()==1
+                      && sb.Any(
+                        v=>v.HomeTeam.Equals(homeTeam) && v.AwayTeam.Equals(awayTeam) 
+                        && v.HomeScore==0 && v.AwayScore==0)
+                      );
+        
+        Debug.WriteLine("ScoreBoard_StartGame_Test End...");
+    }
 }
